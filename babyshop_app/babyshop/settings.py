6d2c3a2e32d20f9f92ee1922ae3a282ce84d18cc
Mike Meyer <mike.meyer86@web.de>
2024-07-03 20:34:35 +0200

    docker: add comments to dockerfile

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Allowed hosts for running app on server and local
+# Allowed hosts for running app on server and localhost
 SERVER_HOST = os.environ.get("DJANGO_ALLOWED_HOST")
 ALLOWED_HOSTS = [SERVER_HOST, "localhost"]
 
